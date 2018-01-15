403fa0d24ea318cff0a8d637bf9ee484d44bc379
FernandoSAguiarNeto <fsan110792@gmail.com>
2018-01-15 17:46:06 -0200

    biClusteringFiltering

--- coclustering/paco.py
+++ coclustering/paco.py
@@ -15,6 +15,7 @@ from caserec.utils.read_file import ReadFile
 from scipy.spatial.distance import squareform, pdist
 from sklearn.cluster import KMeans
 import numpy as np
+import itertools
 
 __author__ = 'Arthur Fortes'
 
@@ -42,12 +43,26 @@ class PaCo(object):
 
     def run_kmeans(self):
         # Call kmeans to rows and cols
-        rows = KMeans(n_clusters=self.k_row).fit(self.training_set['matrix'])
-        cols = KMeans(n_clusters=self.k_col).fit(self.training_set['matrix'].T)
+        clusters_rows = KMeans(n_clusters=self.k_row).fit(self.training_set['matrix'])
+        clusters_cols = KMeans(n_clusters=self.k_col).fit(self.training_set['matrix'].T)
 
         # Map inverse index
-        [self.list_row[label].append(row_id) for row_id, label in enumerate(rows.labels_)]
-        [self.list_col[label].append(col_id) for col_id, label in enumerate(cols.labels_)]
+        [self.list_row[label].append(row_id) for row_id, label in enumerate(clusters_rows.labels_)]
+        [self.list_col[label].append(col_id) for col_id, label in enumerate(clusters_cols.labels_)]
+
+    def count_information(self):
+        for label_row in range(self.k_row):
+            for label_col in range(self.k_col):
+                count_local = 0
+
+                for pair in itertools.product(self.list_row[label_row], self.list_col[label_col]):
+                    if self.training_set['matrix'][pair[0]][pair[1]] != 0:
+                        count_local += 1
+
+                self.count_total.append(len(self.list_row[label_row]) * len(self.list_col[label_col]))
+                self.count_ones.append(count_local)
+
+        self.update_information(first_iteration=True)
 
     def update_information(self, first_iteration=False):
         """
@@ -55,27 +70,23 @@ class PaCo(object):
         """
 
         if first_iteration:
-            self.density = np.matrix(np.divide(self.count_ones, self.count_total)).reshape((self.k_row, self.k_col))
             self.count_total = np.matrix(self.count_total).reshape((self.k_row, self.k_col))
             self.count_ones = np.matrix(self.count_ones).reshape((self.k_row, self.k_col))
+            self.density = np.matrix(np.divide(self.count_ones, self.count_total))
+            # self.density = np.matrix(np.divide(self.count_ones, self.count_total)).reshape((self.k_row, self.k_col))
         else:
             self.density = np.matrix(np.divide(self.count_ones, self.count_total))
             self.density[self.density < self.density_low] = .0
 
-    def count_information(self):
-        for label_row in range(self.k_row):
-            for label_col in range(self.k_col):
-                count_local = 0
+    def calculate_entropy(self):
+        total_density = self.density.sum()
+        probability = np.divide(self.density, total_density)
 
-                pairs = zip(self.list_row[label_row], self.list_col[label_col])
-                for u_id, i_id in pairs:
-                    if self.training_set['matrix'][u_id][i_id] != 0:
-                        count_local += 1
+        sum_pi = 0
+        for pi in probability.flat:
+            sum_pi += 0 if pi == 0 else pi * np.log2(pi)
 
-                self.count_total.append(len(self.list_row[label_row]) * len(self.list_col[label_col]))
-                self.count_ones.append(count_local)
-
-        self.update_information(first_iteration=True)
+        return (-sum_pi) / np.log2(probability.size)
 
     @staticmethod
     def return_min_value(matrix):
@@ -128,16 +139,6 @@ class PaCo(object):
 
         self.update_information()
 
-    def calculate_entropy(self):
-        total_density = self.density.sum()
-        probability = np.divide(self.density, total_density)
-
-        sum_pi = 0
-        for pi in probability.flat:
-            sum_pi += 0 if pi == 0 else pi * np.log2(pi)
-
-        return (-sum_pi) / np.log2(probability.size)
-
     def train_model(self):
         count_epoch = 0
         criteria = True
@@ -150,7 +151,7 @@ class PaCo(object):
 
         # 3st step: training the algorithm
         while criteria:
-            old_list_row, old_list_col = self.list_row.copy(), self.list_col.copy()
+            old_density, old_list_row, old_list_col = self.density.copy(), self.list_row.copy(), self.list_col.copy()
             distance_rows = np.divide(np.float32(squareform(pdist(self.density, 'euclidean'))), self.density.shape[1])
             distance_cols = np.divide(np.float32(squareform(pdist(self.density.T, 'euclidean'))), self.density.shape[0])
             min_row = self.return_min_value(distance_rows)
@@ -159,7 +160,7 @@ class PaCo(object):
             self.merge(min_row, min_col)
 
             # Check the number os bi-clusters
-            if len(self.list_row) == 1 and len(self.list_col):
+            if len(self.list_row) == 1 and len(self.list_col) == 1:
                 break
 
             entropy = self.calculate_entropy()
@@ -167,21 +168,48 @@ class PaCo(object):
             self.delta_entropy.append(dif_entropy)
             mean_range, std_range = np.mean(self.delta_entropy), np.std(self.delta_entropy)
 
-            if not mean_range - 3 * std_range <= dif_entropy <= mean_range + 3 * std_range:
-                self.list_row, self.list_col = old_list_row, old_list_col
-                self.update_information()
+            if not (mean_range - 3 * std_range <= dif_entropy <= mean_range + 3 * std_range):
+                self.density, self.list_row, self.list_col = old_density, old_list_row, old_list_col
                 criteria = False
             else:
-                print('Epoch:: ', count_epoch, " | Entropy:: ", entropy0)
+                print('Epoch:: ', count_epoch, " | Entropy:: ", entropy)
                 entropy0 = entropy
                 count_epoch += 1
 
         return entropy0
 
+    def return_bi_groups(self):
+        print(self.density[np.logical_and(self.density != 1, self.density != 0)])
+
+	# filters the bigroups removing the ones with lower density, leaving the minimum to recommend to every user
+    def filter_bi_groups(self):
+        filteredDensities = self.density.copy()
+        filteredDensities[filteredDensities == 1] = np.nan
+
+        firstRun = True
+        old = filteredDensities.copy()
+        while (True):
+            for line in filteredDensities:
+                if np.nansum(line) == 0:
+                    if firstRun:
+                        return not np.isnan(self.density)
+                    else:
+                        return not np.isnan(old)
+
+            old = filteredDensities.copy()
+
+            index = np.argmin(filteredDensities)
+            line = index // filteredDensities.shape[1]
+            col = index % filteredDensities.shape[1]
+
+            filteredDensities[line, col] = np.nan
+
+            firstRun = False
+		
     def execute(self):
         print("Final entropy::", self.train_model())
         print("Number of rows:: ", len(self.list_row), "Number of columns:: ", len(self.list_col))
         print("Number of bi-groups:: ", len(self.list_row) * len(self.list_col))
         print("Number of bi-groups needing recommendations:: ", self.density[np.logical_and(self.density != 1,
                                                                                             self.density != 0)].size)
-#
+        self.return_bi_groups()
