db0d8f8528b56054581c2b4588b2659f627876e3
Arthur Fortes da Costa <costaaf@motorola.com>
2019-08-20 17:28:43 -0300

    Update Kmedoids

--- clustering/kmedoids.py
+++ clustering/kmedoids.py
@@ -1,47 +1,98 @@
+from scipy.spatial.distance import squareform, pdist
+from sklearn.metrics import silhouette_score
 import numpy as np
 
 
-def kmedoids(distance_matrix, k, max_interactions=10000):
-    # determine dimensions of distance matrix
-    row, col = distance_matrix.shape
+class KMedoids(object):
+    def __init__(self, n_clusters=8, max_iter=1000, verbose=0, random_state=None, metric='euclidean', algorithm='pam'):
+        self.n_clusters = n_clusters
+        self.max_iter = max_iter
+        self.verbose = verbose
+        self.metric = metric
+        self.algorithm = algorithm
 
-    if k > col:
-        raise Exception("Error:: Too many medoids")
+        self.dict_clusters = None
+        self.map_medoids = None
+        self.labels = None
+        self.inertia = None
+        self.dist_x = None
 
-    # randomly initialize an array of k-medoid indices
-    support_matrix = np.arange(col)
-    np.random.shuffle(support_matrix)
-    support_matrix = np.sort(support_matrix[:k])
+        if random_state is not None:
+            np.random.seed(random_state)
 
-    # create a copy of the array of medoid indices
-    new_support_matrix = np.copy(support_matrix)
+    def fit(self, X):
+        self.dist_x = self.fit_transform(X)
 
-    # initialize a dictionary to represent clusters
-    clusters = {}
+        rows, cols = self.dist_x.shape
+        if self.n_clusters > cols:
+            raise Exception("Error:: Too many medoids")
 
-    for _ in range(max_interactions):
-        # determine clusters, i. e. arrays of data indices
-        j_vector = np.argmin(distance_matrix[:, support_matrix], axis=1)
-        for label in range(k):
-            clusters[label] = np.where(j_vector == label)[0]
+        if self.algorithm == 'pam':
+            support_matrix, self.dict_clusters = self.kmedoids_pam(self.dist_x)
+            self.map_medoids = {idx: value for idx,
+                                value in enumerate(support_matrix)}
+        else:
+            raise Exception("Error:: Approach not implemented")
 
-        # update cluster medoids
-        for label in range(k):
-            j_vector = np.mean(distance_matrix[np.ix_(clusters[label], clusters[label])], axis=1)
-            j = np.argmin(j_vector)
-            new_support_matrix[label] = clusters[label][j]
-        np.sort(new_support_matrix)
+        self.labels = np.zeros(rows)
 
-        # check for convergence
-        if np.array_equal(support_matrix, new_support_matrix):
-            break
-        support_matrix = np.copy(new_support_matrix)
+        for key in self.dict_clusters:
+            for idx in self.dict_clusters[key]:
+                self.labels[idx] = key
 
-    else:
-        # final update of cluster memberships
-        j_vector = np.argmin(distance_matrix[:, support_matrix], axis=1)
-        for label in range(k):
-            clusters[label] = np.where(j_vector == label)[0]
+    def fit_predict(self, X):
+        self.fit(X)
+        self.inertia = self.compute_sse(X)
+        return self.map_medoids, self.dict_clusters, self.labels, self.inertia
 
-    # return results
-    return support_matrix, clusters
+    def fit_transform(self, X):
+        return squareform(pdist(X, metric=self.metric))
+
+    def kmedoids_pam(self, distance_matrix):
+        # randomly initialize an array of k-medoid indices
+        support_matrix = np.arange(distance_matrix.shape[1])
+        np.random.shuffle(support_matrix)
+        support_matrix = np.sort(support_matrix[:self.n_clusters])
+
+        # create a copy of the array of medoid indices
+        new_support_matrix = np.copy(support_matrix)
+
+        # initialize a dictionary to represent clusters
+        clusters = {}
+
+        for _ in range(self.max_iter):
+            # determine clusters, i. e. arrays of data indices
+            j_vector = np.argmin(distance_matrix[:, support_matrix], axis=1)
+            for label in range(self.n_clusters):
+                clusters[label] = np.where(j_vector == label)[0]
+
+            # update cluster medoids
+            for label in range(self.n_clusters):
+                j_vector = np.mean(distance_matrix[np.ix_(
+                    clusters[label], clusters[label])], axis=1)
+                j = np.argmin(j_vector)
+                new_support_matrix[label] = clusters[label][j]
+            np.sort(new_support_matrix)
+
+            # check for convergence
+            if np.array_equal(support_matrix, new_support_matrix):
+                break
+            support_matrix = np.copy(new_support_matrix)
+
+        else:
+            # final update of cluster memberships
+            j_vector = np.argmin(distance_matrix[:, support_matrix], axis=1)
+            for label in range(self.n_clusters):
+                clusters[label] = np.where(j_vector == label)[0]
+
+        # return results
+        return support_matrix, clusters
+
+    def compute_sse(self, X):
+        sse = 0.0
+        for c in self.map_medoids:
+            temp = 0.0
+            for p in self.dict_clusters[c]:
+                temp += self.dist_x[self.map_medoids[c], p]
+                sse += temp
+        return sse
